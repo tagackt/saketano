000221f6cb427eaf69300110a63d2d8612dde0df
taga <taga@321sa.net>
2014-12-15 01:20:03 +0900

    change userのカラム定義を変更

--- db/migrate/20141019141738_create_users.rb
+++ db/migrate/20141019141738_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.string :sex
+      t.integer :sex
       t.date :birthday
       t.integer :birth_prefecture_id
       t.integer :current_prefecture_id
--- db/migrate/20141203171111_change_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnToUsers < ActiveRecord::Migration
-  def change
-    change_column :users, :sex, :integer
-  end
-end
