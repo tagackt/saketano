847b1e683b398819d1f18f5e876779f39fe61429
taga <taga@321sa.net>
2014-12-15 02:03:34 +0900

    fix ActionView::Template::Error (uninitialized constant User::Relationship):エラー対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   has_many :impressions
 
-  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :relationships, class_name: relationship, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
   has_many :reverse_relationships, foreign_key: "followed_id", class_name:  "Relationship", dependent: :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
